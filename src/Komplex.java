20e3fe8fd0d99090f6aee63b9b3a8bad86768be3
TheDeniableToast <filipnorberg02@outlook.com>
2020-02-06 11:15:32 +0100

    klart torsdag v6

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,6 +1,18 @@
 public class Komplex {
 
-public static void main(String args[]) {
+    private double re
+    private double im
+
+    public Komplex add( Komplex b) {}
+    public Komplex multiply(Komplex b) {}
+
+    public static void main(String args[]) {
+
+    Komplex a = new Komplex(3.0, 2.0);
+    Komplex b = new Komplex(4.0, 1.0);
+    Komplex c = a.add(b);
+    Komplex d = a.multiply(b);
+
 
 
 }
